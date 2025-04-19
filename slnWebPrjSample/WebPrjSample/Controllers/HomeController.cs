9dfda289d857867bfed7995cfe272fd9394ab5a5
Mephisto Lin <Mephisto007linlin@outlook.com>
2025-04-20 04:56:20 +0800

    [新增]  新增 Controllers

--- slnWebPrjSample/WebPrjSample/Controllers/HomeController.cs
+++ slnWebPrjSample/WebPrjSample/Controllers/HomeController.cs
@@ -3,28 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using WebPrjSample.Models;
+
 
 namespace WebPrjSample.Controllers
 {
     public class HomeController : Controller
     {
+        WebPrjSampleEntities3 db = new WebPrjSampleEntities3();
         public ActionResult Index()
         {
-            return View();
+            var product = db.tProduct.OrderByDescending(m => m.fId).ToList();
+            return View(product);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
