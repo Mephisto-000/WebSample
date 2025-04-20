e8636d0d72218a837ba20b8aff836753a4a68d74
Mephisto Lin <Mephisto007linlin@outlook.com>
2025-04-20 23:01:37 +0800

    [新增]  新增加入購物車, 刪除購物車 的方法

--- slnWebPrjSample/WebPrjSample/Controllers/MemberController.cs
+++ slnWebPrjSample/WebPrjSample/Controllers/MemberController.cs
@@ -25,5 +25,47 @@ namespace WebPrjSample.Controllers
             FormsAuthentication.SignOut();
             return RedirectToAction("Login", "Home");
         }
+
+        public ActionResult ShoppingCar()
+        {
+            string fUserUId = User.Identity.Name;
+            var orderDetails = db.tOrderDetail.Where(m => m.fUserId == fUserUId && m.fIsApproved == "否").ToList();
+            return View(orderDetails);
+        }
+
+        public ActionResult AddCar(string fPId)
+        {
+            string fUserId = User.Identity.Name;
+            var currentCar = db.tOrderDetail.Where(m => m.fPId == fPId && m.fIsApproved == "否"
+                                                                       && m.fUserId == fUserId).FirstOrDefault();
+            if (currentCar != null)
+            {
+
+                var product = db.tProduct.Where(m => m.fPId == fPId).FirstOrDefault();
+
+                tOrderDetail orderDetail = new tOrderDetail();
+                orderDetail.fUserId = fUserId;
+                orderDetail.fPId = product.fPId;
+                orderDetail.fName = product.fName;
+                orderDetail.fPrice = product.fPrice;
+                orderDetail.fQty = 1;
+                orderDetail.fIsApproved = "否";
+                db.tOrderDetail.Add(orderDetail);
+            }
+            else
+            {
+                currentCar.fQty += 1;
+            }
+            db.SaveChanges();
+            return RedirectToAction("ShoppingCar");
+        }
+
+        public ActionResult DeleteCar(int fId)
+        {
+            var orderDetail = db.tOrderDetail.Where(m => m.fId == fId).FirstOrDefault();
+            db.tOrderDetail.Remove(orderDetail);
+            db.SaveChanges();
+            return RedirectToAction("ShoppingCar");
+        }
     }
 }
