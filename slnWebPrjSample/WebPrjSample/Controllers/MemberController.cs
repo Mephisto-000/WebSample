dcb1e80665df020b7cf41fac4bf71bcc27219875
Mephisto Lin <Mephisto007linlin@outlook.com>
2025-04-20 14:56:50 +0800

    [新增]  新增登出的方法

---
+++ slnWebPrjSample/WebPrjSample/Controllers/MemberController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security;
+using WebPrjSample.Models;
+using System.Web.Security;
+
+namespace WebPrjSample.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+        WebPrjSampleEntities db = new WebPrjSampleEntities();
+        // GET: Member
+        public ActionResult Index()
+        {
+            var products = db.tProduct.OrderByDescending(m => m.fId).ToList();
+            return View("../Home/Index","_LayoutMember",products);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
